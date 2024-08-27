8ff5f3ff75d55285c0b6077c81a2a82e90740d13
SrDevel <david.villda@correo.tdea.edu.co>
2024-08-26 20:00:14 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { useState} from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from "react-native";
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/Keyboard';
+export default function App(){
+  const [theme, setTheme] = useState('light');
+  return(
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={theme === 'dark' ? "#f5dd4b" : "#f4f3f4"}
+          style={{marginTop: 20}} 
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          ios_backgroundColor="black"
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'flex-start',
+  }
+})
